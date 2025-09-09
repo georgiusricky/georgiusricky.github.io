f90a80975120307a385126925a31e288cf72b0de
Ricky <ricky.georgius@gmail.com>
2025-09-09 13:50:48 +0700

    fix: add basePath

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  // 👇 USE basepath for github pages
+  basePath: '/georgiusricky.github.io', // or just '/'
 };
-
 export default nextConfig;
