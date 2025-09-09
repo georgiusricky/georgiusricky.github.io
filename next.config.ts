d15de13eb05085fcaea756fc2792204b8c09bcde
Ricky <ricky.georgius@gmail.com>
2025-09-09 13:16:14 +0700

    fix: static file error

--- next.config.ts
+++ next.config.ts
@@ -10,25 +10,6 @@ const nextConfig: NextConfig = {
   generateBuildId: async () => {
     return 'build-' + Date.now();
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.output.filename = 'static/js/[name].[contenthash].js';
-      config.output.chunkFilename = 'static/js/[name].[contenthash].chunk.js';
-      
-      if (config.optimization && config.optimization.splitChunks) {
-        config.optimization.splitChunks.cacheGroups = {
-          ...config.optimization.splitChunks.cacheGroups,
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all',
-            priority: 10,
-          },
-        };
-      }
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
