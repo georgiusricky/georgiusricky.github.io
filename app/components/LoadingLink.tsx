b04253408a67dbc89c51f79b6dbd3425a036f24f
Ricky <ricky.georgius@gmail.com>
2025-10-02 21:39:03 +0700

    fix: lint unused

--- app/components/LoadingLink.tsx
+++ app/components/LoadingLink.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode, useEffect } from 'react';
+import { ReactNode } from 'react';
 import Link from 'next/link';
 import { useRouter, usePathname } from 'next/navigation';
 import { useLoadingStore } from '@/stores/loadingStore';
