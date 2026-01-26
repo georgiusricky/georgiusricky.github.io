24684702df42ce673b3254bd49cbc70decf07c6a
Ricky <ricky.georgius@gmail.com>
2026-01-26 18:16:04 +0700

    refactor: remove About component and update Hero section with experience details

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,23 +3,32 @@
 import { Github, Linkedin } from 'lucide-react'
 import Link from 'next/link'
 import Image from 'next/image'
-import ProfileCircle from './ProfileCircle'
+import Polaroid from './Polaroid'
 import { useGlobalStore } from '@/stores/globalStore'
 
 export default function Hero() {
   const socials = useGlobalStore((state) => state.socials)
+  const experiences = useGlobalStore((state) => state.experiences)
+
+  const firstYear = Number(experiences.at(-1)?.period.start.split(' ')[1])
+  const endDate = experiences[0]?.period.end
+  const lastYear = endDate === 'Present' ? new Date().getFullYear() : Number(endDate?.split(' ')[1])
+  const totalExpYear = lastYear - firstYear
 
   return (
-    <section className="container mx-auto px-4 pt-32 pb-16">
+    <section className="container mx-auto px-4 pt-56 pb-16">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between">
-        <div className="w-full md:w-8/12 mb-8 md:mb-0">
+        <div className="w-full md:w-6/12 mb-8 md:mb-0">
           <h1 className="text-4xl font-mono font-bold mb-4">
             {`Hello I'm`} <span className="block text-6xl">Ricky</span>
           </h1>
           <h2 className="text-3xl font-bold mb-4 font-mono">
             Software Engineer
           </h2>
-          <div className="text-gray-600 dark:text-gray-400 mb-8 flex items-center">
+          <p className="text-gray-600 dark:text-gray-400 mb-6">
+            {`I'm a passionate web developer who began my journey in 2019. Over ${totalExpYear} years of experience, I've embraced modern development practices, continuously improving my skills to deliver exceptional user experiences. I thrive on taking on new challenges and enjoy learning something new to stay ahead in the ever-evolving tech landscape.`}
+          </p>
+          <div className="text-black dark:text-white mb-8 flex items-center font-semibold">
             <span>Based in Earth</span>
              <Image
                 src="/img/gif/earth.gif"
@@ -58,8 +67,8 @@ export default function Hero() {
             </Link>
           </div>
         </div>
-        <div className="w-8/12 md:w-4/12">
-         <ProfileCircle/>
+        <div className="w-full md:w-5/12 max-w-md">
+          <Polaroid src="/img/underwater.webp" alt="underwater"/>
         </div>
       </div>
     </section>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from './components/Hero'
 import Skills from './components/Skills'
 import Experience from './components/Experience'
-import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
@@ -11,7 +10,6 @@ export default function Home() {
   return (
     <main className="bg-white dark:bg-black">
       <Hero />
-      <div id="about"><About /></div>
       <div id="skills"><Skills /></div>
       <Experience />
       <Projects />
